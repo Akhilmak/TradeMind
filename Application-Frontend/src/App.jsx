c44914da508e26ac57a91855bb3f22930a8049f2
Akhi <akhilkumarmadineni@gmail.com>
2024-09-28 19:06:18 +0530

    Created Frontend NavBar

---
+++ Application-Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Button } from './components/ui/button'
+import Navbar from './Page/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      
+
+      <Navbar />
+      
+    </>
+  )
+}
+
+export default App
---
+++ Application-Frontend/src/Page/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { DragHandleHorizontalIcon } from "@radix-ui/react-icons";
+import { Avatar } from "@/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import Sidebar from "./Sidebar";
+
+const Navbar = () => {
+  return (
+    <div className="px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center">
+      <div className="flex items-center gap-3">
+        <Sheet>
+          <SheetTrigger>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full w-11 h-11"
+            >
+              <DragHandleHorizontalIcon className="h-7 w-7" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            className="w-72 border-r-0 flex flex-col justify-center"
+            side="left"
+          >
+            <SheetHeader>
+              <SheetTitle>
+                <div className="text-3xl flex justify-center items-center gap-1">
+                  <Avatar>
+                    <AvatarImage src="https://cdn.pixabay.com/photo/2015/08/27/11/20/bitcoin-910307_1280.png"></AvatarImage>
+                  </Avatar>
+                  <div>
+                    <span className="font-bold text-red-500">Trade</span>
+                    <span>Mind</span>
+                  </div>
+                </div>
+              </SheetTitle>
+              <SheetDescription>
+                
+              </SheetDescription>
+            </SheetHeader>
+            <Sidebar/>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
