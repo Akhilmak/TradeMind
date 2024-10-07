5b70bc9b1a0f0ebc15e6ecf1cbf415f34f39bdb8
Akhi <akhilkumarmadineni@gmail.com>
2024-10-07 22:25:45 +0530

    .

--- Application-Frontend/src/Page/Wallet/TopupForm.jsx
+++ Application-Frontend/src/Page/Wallet/TopupForm.jsx
@@ -1,16 +1,32 @@
-import { Input } from '@/components/ui/input'
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { RadioGroup } from "@/components/ui/radio-group";
+import React from "react";
 
 const TopupForm = () => {
+  const [amount, setAmount] = React.useState("");
+  const handleChange = (e) => {
+    setAmount(e.target.value);
+  };
   return (
-    <div className='pt-10 space-y-5'>
-        <div>
-            <h1 className='pb-1'>Enter Amount</h1>
-        </div>
-<Input className='py-7 text-lg' placeholder="$9999">
-</Input>
-    </div>
-  )
-}
+    <div className="pt-10 space-y-5">
+      <div>
+        <h1 className="pb-1">Enter Amount</h1>
+      
+      <Input
+        onChange={handleChange}
+        value={amount}
+        className="py-7 text-lg"
+        placeholder="$9999"
+      ></Input>
+      </div>
+      <div>
+        <h1 className="pb-1">Select Payment Method</h1>
+        <RadioGroup className='flex' defaultValue="RAZORPAy">
 
-export default TopupForm
+        </RadioGroup>
+      </div>
+    </div>
+  );
+};
+
+export default TopupForm;
