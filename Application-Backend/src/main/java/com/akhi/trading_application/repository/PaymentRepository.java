64a72984bb0280031217135aec298599c57e39f1
Akhi <97934892+Akhilmak@users.noreply.github.com>
2024-10-29 23:21:55 +0530

    .

--- Application-Backend/src/main/java/com/akhi/trading_application/repository/PaymentRepository.java
+++ Application-Backend/src/main/java/com/akhi/trading_application/repository/PaymentRepository.java
@@ -3,7 +3,9 @@ package com.akhi.trading_application.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.akhi.trading_application.modal.PaymentOrder;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<PaymentOrder, Long> {
 
 }
